cc931a1a2917f4b2115bd8683ed8ba2c46a2e2b9
CRCRCRCRCRC <corrty12345678901234567890@gmail.com>
2025-08-17 20:40:48 +0800

    update

---
+++ api/video/info.ts
@@ -0,0 +1,7 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import app from '../app.js';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  return (app as unknown as (req: any, res: any) => void)(req, res);
+}
+
